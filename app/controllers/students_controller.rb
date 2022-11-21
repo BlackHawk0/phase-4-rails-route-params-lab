24c0a2581b7b85033a25840dfb05391ff91d4ad6
Wanambuko Ngoya Daniel <danielngoys@outlook.com>
2022-11-21 17:18:07 +0300

    query string parameters

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,12 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
-    render json: students
+    if params[:name]
+      @students = Student.where('lower(first_name) = ? OR lower(last_name) = ?', params[:name].downcase, params[:name].downcase)
+    else
+      @students = Student.all
+    end
+    render json: @students
   end
 
   def show
